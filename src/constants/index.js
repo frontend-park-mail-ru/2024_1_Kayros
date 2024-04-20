8adfeee1be0b90ba3f7a95c74c25359596cf4580
novakf <bogdannovikov116@mail.ru>
2024-04-20 11:17:29 +0300

    update api key

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,4 +173,4 @@ export const ENTRANCE_REGEX = /^[1-9][0-9]{0,1}$/;
 
 export const YANDEX_API_SAGESTS = 'f96d8d36-1d0a-4786-9140-1b350e8179e1';
 
-export const YANDEX_API_GEOCODER = '0b04a1cd-dc01-4c43-b44a-3efac84fa9f6';
+export const YANDEX_API_GEOCODER = '7e5782d5-d181-428b-a46d-5dcff8caa54d';
