9342f2fac9740e7f442c11d7a7782cb1ed9692c9
novakf <bogdannovikov116@mail.ru>
2024-05-25 22:34:47 +0300

    change build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd /home/ubuntu/frontend
+git -C /home/ubuntu/frontend pull
 npm i
 npm run prebuild
-cp -r dist/ /var/www/
-service nginx restart
-echo "DONE"
+cp -r ./dist /var/www
+sudo systemctl restart nginx
+echo "====== DONE ======"
